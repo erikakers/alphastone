8af2e37f930b34408afab0ec106151be665ab865
Erik Akers <erikakers@gmail.com>
2013-09-18 21:10:20 -0500

    app features init function

--- src/javascript/init.js
+++ src/javascript/init.js
@@ -2,6 +2,7 @@
 // Initialize any features that aren't Initialzied through data-features method
 App.init = function() {
     App.Helpers.AssetsLoader();
+    App.Features.init();
 };
 
 // Kick it all off on DOM Ready
