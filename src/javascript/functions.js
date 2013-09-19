779bf3522d5acd94596456dce44bc1ae155dff26
Erik Akers <erikakers@gmail.com>
2013-09-18 21:21:08 -0500

    adding functions file

---
+++ src/javascript/functions.js
@@ -0,0 +1,8 @@
+// This object is for globally repeated functions that are used more the once.
+// Remove this file form the Gruntfile.js file if it is not used.
+
+App.Functions = {
+	method: function() {
+		//Do something
+	}
+};
