8a051486b200522ebb5233f96725a3416f24f6e0
Erik Akers <erikakers@gmail.com>
2013-10-02 17:06:16 -0500

    updating comments

--- src/javascript/functions.js
+++ src/javascript/functions.js
@@ -1,4 +1,4 @@
-// This object is for globally repeated functions that are used more the once.
+// This object is for globally repeated functions namespaced with App.Functions.
 // Remove this file form the Gruntfile.js file if it is not used.
 
 App.Functions = {
