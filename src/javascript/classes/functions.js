c9be7a56e8b7028f69a26b437782d542aecb3094
Erik Akers <erikakers@gmail.com>
2014-01-03 16:35:24 -0600

    moving to functions.js file to classes folder

---
+++ src/javascript/classes/functions.js
@@ -0,0 +1,8 @@
+// This is setting up an OOP like structure for repeating functions.
+
+
+App.Functions = {
+	method: function() {
+		//Do something
+	}
+};
--- src/javascript/functions.js
+++
@@ -1,8 +0,0 @@
-// This object is for globally repeated functions namespaced with App.Functions.
-// Remove this file form the Gruntfile.js file if it is not used.
-
-App.Functions = {
-	method: function() {
-		//Do something
-	}
-};
