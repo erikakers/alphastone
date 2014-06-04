eac577f62c9609c1efc5d722392b85a8ae0ddcc6
Erik Akers <erikakers@gmail.com>
2014-06-04 13:36:00 -0500

    adding grunt concat options

--- grunt/options/concat.js
+++ grunt/options/concat.js
@@ -10,18 +10,25 @@ module.exports = {
 	},
 	vendor: {
 		src: [
-			'<%= config.vendor %>/jquery/dist/jquery.js'
+			'<%= config.vendor %>/jquery/dist/jquery.js',
+			'<%= config.vendor %>/jquery-tiny-pubsub/dist/ba-tiny-pubsub.js',
+			'<%= config.vendor %>/lodash/dist/lodash.js',
+			'<%= config.vendor %>/velocity/jquery.velocity.js'
 		],
 		dest: '<%= config.app %>/scripts/vendor/plugins.js'
 	},
 	dev: {
 		src: [
+			'<%= config.src %>/javascript/namespace.js',
+			'<%= config.src %>/javascript/constants.js',
+			'<%= config.src %>/javascript/featureLoader.js',
 			'<%= config.src %>/javascript/classes/{,*/}*.js',
 			'<%= config.src %>/javascript/helpers/{,*/}*.js',
 			'<%= config.src %>/javascript/features/{,*/}*.js',
 			'<%= config.src %>/javascript/models/{,*/}*.js',
 			'<%= config.src %>/javascript/controllers/{,*/}*.js',
-			'<%= config.src %>/javascript/views/{,*/}*.js'
+			'<%= config.src %>/javascript/views/{,*/}*.js',
+			'<%= config.src %>/javascript/init.js'
 		],
 		dest: '<%= config.app %>/scripts/main.js'
 	}
