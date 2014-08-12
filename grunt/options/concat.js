0666106e421d191627bfa4b719eb9953d5638333
Erik Akers <erikakers@gmail.com>
2014-08-12 12:45:37 -0500

    updating grunt concat task

--- grunt/options/concat.js
+++ grunt/options/concat.js
@@ -2,33 +2,32 @@ module.exports = {
 	options: {
 		banner: '<%= banner %>'
 	},
-	head: {
+	loader {
 		src: [
-			// Any Shims
+			'<%= config.src %>/javascript/loader/**/*.js'
 		],
-		dest: '<%= config.app %>/scripts/vendor/head.js'
+		dest: '<%= config.src %>/scripts/loader.js'
 	},
 	vendor: {
 		src: [
-			'<%= config.vendor %>/jquery/dist/jquery.js',
-			'<%= config.vendor %>/jquery-tiny-pubsub/dist/ba-tiny-pubsub.js',
-			'<%= config.vendor %>/lodash/dist/lodash.js',
-			'<%= config.vendor %>/velocity/jquery.velocity.js'
+			'<%= config.vendor %>/jquery/dist/jquery.js'
 		],
 		dest: '<%= config.app %>/scripts/vendor/plugins.js'
 	},
+	mobile: {
+		src: [
+			'<%= config.vendor %>/fastclick/lib/fastclick.js'
+		],
+		dest: '<%= config.app %>/mobile/vendor/mobile.js'
+	},
 	dev: {
 		src: [
-			'<%= config.src %>/javascript/namespace.js',
-			'<%= config.src %>/javascript/constants.js',
-			'<%= config.src %>/javascript/featureLoader.js',
 			'<%= config.src %>/javascript/classes/{,*/}*.js',
 			'<%= config.src %>/javascript/helpers/{,*/}*.js',
 			'<%= config.src %>/javascript/features/{,*/}*.js',
 			'<%= config.src %>/javascript/models/{,*/}*.js',
 			'<%= config.src %>/javascript/controllers/{,*/}*.js',
-			'<%= config.src %>/javascript/views/{,*/}*.js',
-			'<%= config.src %>/javascript/init.js'
+			'<%= config.src %>/javascript/views/{,*/}*.js'
 		],
 		dest: '<%= config.app %>/scripts/main.js'
 	}
