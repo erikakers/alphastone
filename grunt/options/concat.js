b11f75fd176baaddab9ec79a55d5a63f197f9c16
Erik Akers <erikakers@gmail.com>
2015-03-02 13:34:10 -0600

    how was that forgotten?

--- grunt/options/concat.js
+++ grunt/options/concat.js
@@ -19,19 +19,23 @@ module.exports = {
 	mobile: {
 		src: [
 			// Any Mobile specific libraries should be added here.
-			// If added via bower use <%= config.vendor %> short cut that maps to the 
+			// If added via bower use <%= config.vendor %> short cut that maps to the
 			// bower created directory
 		],
 		dest: '<%= config.app %>/mobile/vendor/mobile.js'
 	},
 	dev: {
 		src: [
+			'<%= config.src %>/javascript/namespace.js',
+			'<%= config.src %>/javascript/constants.js',
+			'<%= config.src %>/javascript/featuresLoader.js',
 			'<%= config.src %>/javascript/classes/{,*/}*.js',
 			'<%= config.src %>/javascript/helpers/{,*/}*.js',
 			'<%= config.src %>/javascript/features/{,*/}*.js',
 			'<%= config.src %>/javascript/models/{,*/}*.js',
 			'<%= config.src %>/javascript/controllers/{,*/}*.js',
-			'<%= config.src %>/javascript/views/{,*/}*.js'
+			'<%= config.src %>/javascript/views/{,*/}*.js',
+			'<%= config.src %>/javascript/init.js'
 		],
 		dest: '<%= config.app %>/scripts/main.js'
 	}
