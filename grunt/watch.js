5b2f0bb112067a6225f24fbd2c72c66c799cac96
Erik Akers <erikakers@gmail.com>
2015-04-10 14:39:53 -0500

    removing live reload and connect server

--- grunt/watch.js
+++ grunt/watch.js
@@ -18,16 +18,5 @@ module.exports = {
 	mocha: {
 		files: ['test/{,*/}*.js'],
 		tasks: ['mocha']
-	},
-	livereload: {
-		options: {
-			livereload: '<%= connect.options.livereload %>'
-		},
-		files: [
-			'<%= config.app %>/**/*.html',
-			'<%= config.app %>/styles/{,*/}*.css',
-			'{.tmp,<%= config.app %>}/scripts/{,*/}*.js',
-			'<%= config.app %>/images/{,*/}*.{gif,jpeg,jpg,png,svg,webp}'
-		]
 	}
 };
