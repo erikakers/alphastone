fcae1b071b99d3cafa858ae59b0e8c4200587d87
Erik Akers <erikakers@gmail.com>
2015-09-29 19:22:57 -0500

    cleaning up file for from linting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,30 @@
 module.exports = function(grunt) {
-    "use strict";
+  var project = {
+    config: {
+      src: 'src',
+      build: 'app',
+      dist: 'dist',
+      temp: '.temp',
+      vendor: grunt.file.readJSON('.bowerrc').directory
+    },
+    paths: {
+      javascript: 'src/javascript',
+      scss: 'src/scss',
+      html: 'src/html',
+      vendor: grunt.file.readJSON('.bowerrc').directory,
+      build: {
+        script: 'app/scripts',
+        styles: 'app/styles'
+      }
+    }
+  };
 
-    var project = {
-        config: {
-            src: 'src',
-            build: 'app',
-            dist: 'dist',
-            temp: '.temp',
-            vendor: grunt.file.readJSON('.bowerrc').directory
-        },
-        paths: {
-            javascript: 'src/javascript',
-            scss: 'src/scss',
-            html: 'src/html',
-            vendor: grunt.file.readJSON('.bowerrc').directory,
-            build : {
-                script: 'app/scripts',
-                styles: 'app/styles'
-            }
-        }
-    };
+  require('load-grunt-config')(grunt, {
+    data: project,
+    jitGrunt: {
+      staticMappings: {}
+    }
+  });
 
-    require('load-grunt-config')(grunt, {
-        data: project,
-        jitGrunt: {
-            staticMappings: {
-            }
-        }
-    });
-
-    require('time-grunt')(grunt);
+  require('time-grunt')(grunt);
 };
